34de76ed5f025034456aad7eb7d6348019f1495f
Agility User <f5examplecom@gmail.com>
2019-08-12 18:05:01 -0400

    cleaning this up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # playbooks
+
+
+docker pull examplecom/shared:ansible-playbook
+
+docker run --rm -it -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub  -v $(pwd):/ansible examplecom/shared:ansible-playbook init.yml
--- docker-ansible-playbook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-
-sudo docker run --rm -it -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub  -v $(pwd)/playbooks:/ansible/playbooks -v $(pwd)/inventory:/ansible/inventory -v $(pwd)/roles:/ansible/roles -v $(pwd)/etc:/ansible/etc -v $(pwd)/log:/ansible/log examplecom/shared:ansible-playbook init.yml
-
